72c2cf91fa477e7bea79e83ec402fb3f9b639079
alacwong <mr6902spades@gmail.com>
2021-04-27 01:55:57 -0400

    feat: add image search endpoint

--- blueprints/images/controllers/image_resources.py
+++ blueprints/images/controllers/image_resources.py
@@ -5,18 +5,34 @@ These endpoints can be reached at /images/search/.
 """
 from flask_apispec import marshal_with, doc
 
-from ..schema import ImageSchema
+from ..schema import ImagesSchema, args_schema
 from .image_base_resource import ImageBaseResource
+from webargs.flaskparser import use_args
+from ..models.image import Image
 
 
-@doc(description="""Image resource""",)
+@doc(description="""Image resource""", )
 class ImageResource(ImageBaseResource):
 
-    @marshal_with(ImageSchema)
-    def get(self):
+    @marshal_with(ImagesSchema)
+    @use_args(args_schema, location='query')
+    def get(self, args):
         """
         Return a paginated list of images.
         """
-        return {'url': 'url', 'tag': 'tag'}
 
+        tag = args.get('pokemon')
+        page = args.get('page')
+        limit = args.get('limit')
 
+        if not tag:
+            return {}
+
+        if not page:
+            page = 1
+
+        if not limit:
+            limit = 5
+
+        images = list(Image.objects(tag=tag))
+        return {'images': images[page * limit: (page + 1) * limit]}
--- blueprints/images/schema.py
+++ blueprints/images/schema.py
@@ -4,3 +4,14 @@ from marshmallow import Schema, fields
 class ImageSchema(Schema):
     tag = fields.Str()
     url = fields.Str()
+
+
+class ImagesSchema(Schema):
+    images = fields.List(fields.Nested(ImageSchema))
+
+
+args_schema = {
+    "page": fields.Int(description="Page number of table", default=1),
+    "limit": fields.Int(description="How many entries per page", default=5),
+    "pokemon": fields.Str(description='Pokemon image to be searched')
+}
